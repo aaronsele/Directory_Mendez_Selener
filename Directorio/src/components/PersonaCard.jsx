8c6890b02962bf0616e2d05e234a507387d54ad4
aaronsele <48522418@est.ort.edu.ar>
2025-06-18 08:55:31 -0300

    css arreglado fin

--- Directorio/src/components/PersonaCard.jsx
+++ Directorio/src/components/PersonaCard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import './PersonaCard.css'
 function PersonaCard({ persona }) {
   return (
-    <div>
+    <div className='card'>
       <h2>{persona.nombre} {persona.apellido}</h2>
       <p>Edad: {persona.edad}</p>
       <Link to={`/DetallePersona/${persona.id}`}><button>Ver más</button></Link>
