43f782962ad8934cb5b85ec4a38227a4bedab473
Aaronsele <48522418@est.ort.edu.ar>
2025-06-28 20:24:03 -0300

    pages

--- Directorio/vite.config.js
+++ Directorio/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Directory_Mendez_Selener/', // 👈 cambiá esto por el nombre real de tu repo
   plugins: [react()],
 })
